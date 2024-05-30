4ff8263cbd6f98cc026fbf965abf138bc4951386
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-05-30 23:21:30 +0200

    Made the button do something when you click it! It's hooked up to get a new meme image now used prettier to format code

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,26 +1,29 @@
 export default function Meme() {
   return (
     <main>
-      <form className="form">
-        <div>
-          <label htmlFor="top-text">Top text</label>
-          <input 
-          id="top-text"
-          type="text" 
-          placeholder="Shut up" 
-          className="form--input" />
-        </div>
-        <div>
-          <label htmlFor="bottom-text">Bottom text </label>
+      <div className="form">
+        <label htmlFor="top-text">
+          Top text
+          <input
+            id="top-text"
+            type="text"
+            placeholder="Shut up"
+            className="form--input"
+          />
+        </label>
+        <label htmlFor="bottom-text">
+          Bottom text
           <input
             id="bottom-text"
             type="text"
             placeholder="and take my money"
             className="form--input"
           />
-        </div>
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+        </label>
+        <button className="form--button"
+        onClick={getMemeImage}
+        >Get a new meme image 🖼</button>
+      </div>
     </main>
   );
 }
