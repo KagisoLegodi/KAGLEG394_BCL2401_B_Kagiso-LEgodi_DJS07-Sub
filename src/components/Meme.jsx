63be7032ce3bb6f17c0d31b0b3e84e90da8a8a21
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-05-30 18:59:21 +0200

    Used labels to make the text boxes more clear. Realized it's important for accessibility!

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,8 +2,23 @@ export default function Meme() {
   return (
     <main>
       <form className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <div>
+          <label htmlFor="top-text">Top text</label>
+          <input 
+          id="top-text"
+          type="text" 
+          placeholder="Shut up" 
+          className="form--input" />
+        </div>
+        <div>
+          <label htmlFor="bottom-text">Bottom text </label>
+          <input
+            id="bottom-text"
+            type="text"
+            placeholder="and take my money"
+            className="form--input"
+          />
+        </div>
         <button className="form--button">Get a new meme image 🖼</button>
       </form>
     </main>
