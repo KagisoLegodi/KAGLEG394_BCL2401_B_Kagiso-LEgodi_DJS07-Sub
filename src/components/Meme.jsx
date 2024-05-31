cb1ae44100d9ac14dc3f982ccbdd667a64c9eaaf
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-05-31 22:38:03 +0200

    Wrote code to get meme data from the internet when the app starts. The data is now saved in my app so I can use it later

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,19 @@
 import { useState, useEffect } from "react";
 
 export default function Meme() {
-    //const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
-    const [meme, setMeme] = React.useState({
-        topText:"",
-        bottomText:"",
-        randomImage: "http://i.imgflip.com/1bij.jpg"
-    })
-    const [allMemeImages] = React.useState(memesData)
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg",
+    });
+
+    const [allMemes, setAllMemes] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes));
+    }, []); // Empty dependency array ensures this runs only once on mount
 
     function getMemeImage() {
         const memesArray = allMemeImages.data.memes
